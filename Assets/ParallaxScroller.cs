ce401942c2cc20410d720f5e692a3ab3533fefa4
Peter Lukasiewicz <k_iller21@hotmail.com>
2015-06-30 23:24:36 +0200

    Testing Scroller scene

---
+++ Assets/ParallaxScroller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxScroller : MonoBehaviour {
+
+	public float unitsPerSecond = 0f;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 target = transform.position;
+		if (transform.position.x <= 16) {
+			float step = unitsPerSecond * Time.deltaTime;
+			target.Set(target.x + 16, target.y, target.z);
+			transform.position = Vector3.MoveTowards(transform.position, target, 	step);
+		} else {
+			transform.position = transform.position - new Vector3(32, 0, 0);
+		}
+	}
+}
